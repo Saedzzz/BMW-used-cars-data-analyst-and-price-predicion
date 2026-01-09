df6a682a846b31233d2dfee3804d30bd562ad15d
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 21:42:25 +0100

    Update Creating tables.sql

--- SQL steps/Creating tables.sql
+++ SQL steps/Creating tables.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE usedCars_staging (
+    model TEXT,
+    year TEXT,
+    price TEXT,
+    transmission TEXT,
+    mileage TEXT,
+    fuelType TEXT,
+    tax TEXT,
+    mpg TEXT,
+    engineSize TEXT
+);
