716abe8b547ee852dabab759bff1d9771f141f12
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 21:43:48 +0100

    Update Inseting values.sql

--- SQL steps/Inseting values.sql
+++ SQL steps/Inseting values.sql
@@ -1,1 +1,16 @@
-
+--- Inserting the values from the staging table into the actual table
+INSERT INTO usedCars (
+    model, year, price, transmission, mileage,
+    fuelType, tax, mpg, engineSize
+)
+SELECT
+    model,
+    NULLIF(year, '')::INT,
+    NULLIF(price, '')::INT,
+    transmission,
+    NULLIF(mileage, '')::INT,
+    fuelType,
+    NULLIF(tax, '')::INT,
+    NULLIF(mpg, '')::FLOAT,
+    NULLIF(engineSize, '')::FLOAT
+FROM usedCars_staging;
