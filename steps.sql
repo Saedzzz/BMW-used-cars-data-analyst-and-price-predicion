02cbf21efb98c762acca7ffa2edc39b61a95672a
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 20:58:20 +0100

    steps.sql

--- steps.sql
+++ steps.sql
@@ -1,3 +1,4 @@
+--- Creating the staging table 
 CREATE TABLE usedCars_staging (
     model TEXT,
     year TEXT,
@@ -9,3 +10,35 @@ CREATE TABLE usedCars_staging (
     mpg TEXT,
     engineSize TEXT
 );
+
+--- Creating the actual table
+DROP TABLE IF EXISTS usedCars;
+CREATE TABLE usedCars (
+    car_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    model VARCHAR(50),
+    year INT,
+    price INT,
+    transmission VARCHAR(20),
+    mileage INT,
+    fuelType VARCHAR(20),
+    tax INT,
+    mpg FLOAT,
+    engineSize FLOAT
+);
+
+--- Inserting the values from the staging table into the actual table
+INSERT INTO usedCars (
+    model, year, price, transmission, mileage,
+    fuelType, tax, mpg, engineSize
+)
+SELECT
+    model,
+    NULLIF(year, '')::INT,
+    NULLIF(price, '')::INT,
+    transmission,
+    NULLIF(mileage, '')::INT,
+    fuelType,
+    NULLIF(tax, '')::INT,
+    NULLIF(mpg, '')::FLOAT,
+    NULLIF(engineSize, '')::FLOAT
+FROM usedCars_staging;
