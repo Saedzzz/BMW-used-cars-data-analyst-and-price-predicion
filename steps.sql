acb883c81f9c9c408fc136466d889e583cb1cc0c
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 21:07:15 +0100

    SQLworkflow.sql

--- steps.sql
+++ steps.sql
@@ -42,3 +42,11 @@ SELECT
     NULLIF(mpg, '')::FLOAT,
     NULLIF(engineSize, '')::FLOAT
 FROM usedCars_staging;
+
+
+--- Verifying that all the data got imported
+SELECT * FROM usedcars;
+
+--- Verifying that copying the data from the staging table (usedCars_staging) to the main table (usedCars)
+SELECT COUNT(*) FROM usedCars;
+SELECT COUNT(*) FROM usedCars_staging;
