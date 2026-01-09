fa3dab27c27d742d6097afb5a55d4bc2d00547d4
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 21:45:05 +0100

    Update Verifying and cleaning the data.sql

--- SQL steps/Verifying and cleaning the data.sql
+++ SQL steps/Verifying and cleaning the data.sql
@@ -4,3 +4,4 @@ SELECT * FROM usedcars;
 --- Verifying that copying the data from the staging table (usedCars_staging) to the main table (usedCars)
 SELECT COUNT(*) FROM usedCars;
 SELECT COUNT(*) FROM usedCars_staging;
+
