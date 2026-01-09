401cba8054729ba1fd4cb46dd50e99f1c25c3ed0
Saedzzz <155313961+Saedzzz@users.noreply.github.com>
2026-01-09 21:43:01 +0100

    Creating tables.sql

--- SQL steps/Creating tables.sql
+++ SQL steps/Creating tables.sql
@@ -1,3 +1,4 @@
+--- Creating the staging table 
 CREATE TABLE usedCars_staging (
     model TEXT,
     year TEXT,
@@ -10,3 +11,17 @@ CREATE TABLE usedCars_staging (
     engineSize TEXT
 );
 
+--- Creating the actual table
+DROP TABLE IF EXISTS usedCars;
+CREATE TABLE usedCars (
+    car_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    model VARCHAR(50),
+    year INT,
+    price INT,
+    transmission VARCHAR(20),
+    mileage INT,
+    fuelType VARCHAR(20),
+    tax INT,
+    mpg FLOAT,
+    engineSize FLOAT
+);
